571003436349e08326d7e6a0e33b30ff6aecd762
itsterit <itsterit@gmail.com>
2026-05-11 13:23:16 +0400

    first commit

---
+++ main/src/config.h
@@ -0,0 +1,9 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define __PLATFORM__WINDOWS__
+
+#define HEADER_WINDOW_HEIGH 1
+#define BODY_WINDOW_BORDER 1
+
+#endif /* __CONFIG_H__ */
---
+++ main/src/main.hpp
@@ -0,0 +1,11 @@
+#ifndef __MAIN_HPP__
+#define __MAIN_HPP__
+#include "config.h"
+
+#include <iostream>
+#include <windows.h>
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+
+#endif /* __MAIN_HPP__ */
